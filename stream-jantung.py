6a8c35796b1fd52416bcf0f79a9dc696e90cf6fe
prayanaputra <108852536+prayanaputra@users.noreply.github.com>
2024-06-26 14:03:04 +0700

    Update stream-jantung.py

--- stream-jantung.py
+++ stream-jantung.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 
 # load save model
-model = pickle.load(open('penyakit_jantung.sav', 'rb'))
+model = pickle.load(open('penyakit_jantung.sav', 'rb'))  # Ensure to replace 'diabetes_model.sav' with your actual model file
 
 #judul web
 st.title('Prediksi Penyakit Jantung')
